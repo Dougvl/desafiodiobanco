bd667fc208a960d3baafdd714ff0f6460348d412
Douglas <douglassilva@gmail.com>
2022-04-02 23:23:09 -0300

    commit inicial

---
+++ src/desafiodiobanco/Main.java
@@ -0,0 +1,28 @@
+package desafiodiobanco;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Cliente venilton = new Cliente();
+		venilton.setNome("Venilton");
+		
+		Conta cc = new ContaCorrente(venilton);
+		Conta poupanca = new ContaPoupanca(venilton);
+
+		cc.depositar(100);
+		cc.transferir(100, poupanca);
+		
+		cc.imprimirExtrato();
+		poupanca.imprimirExtrato();
+		
+		Cliente dougras = new Cliente();
+		dougras.setNome("Douglas");
+		 cc = new ContaCorrente(dougras);
+		poupanca = new ContaPoupanca(dougras);
+		cc.depositar(1000);
+		cc.transferir(90, poupanca);
+		cc.imprimirExtrato();
+		poupanca.imprimirExtrato();
+	}
+
+}
